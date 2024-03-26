e2d76a8b9243e8ba79d063cd2dbc85f8cc2a669b
Christophe Blonde <cblonde@2A7.42angouleme.fr>
2024-03-26 15:31:39 +0100

    add:env test

---
+++ src/main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 12:21:50 by cblonde           #+#    #+#             */
+/*   Updated: 2024/03/26 17:18:42 by cblonde          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	ft_usage(void)
+{
+	ft_putendl_fd("\033[1;34mUsage: [ minishell ]\033[m", 2);
+	return (1);
+}
+
+int	main(int argc, char *argv[], char *env[])
+{
+	char	**arr_env;
+	int		i;i
+
+	if (!arr)
+		return ;
+	i = 0;
+		ft_putstr_fd(argv[1], 2);
+		ft_putstr_fd(" is not option !\n", 2);
+		return (ft_usage());
+	}
+	arr_env = ft_parse_env(env);
+	if (!arr_env)
+		return (1);
+	while (arr_env[i])
+	{
+		ft_putendl_fd(arr_env[i], 1);
+		i++;
+	}
+	ft_free_array((void **)arr_env);
+	return (0);
