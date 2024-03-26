3326f2b7b9e96230f5868c74d85ef3169b7d103b
Tapeky <tareq.sadouk@gmail.com>
2024-03-26 15:52:12 +0100

    Added builtin dir

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -1,23 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin.h                                          :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/26 15:43:25 by tsadouk           #+#    #+#             */
-/*   Updated: 2024/03/26 15:48:01 by tsadouk          ###   ########.fr       */
+/*   Created: 2024/03/26 15:46:43 by tsadouk           #+#    #+#             */
+/*   Updated: 2024/03/26 15:47:36 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTIN_H
-# define BUILTIN_H
+#include "../include/builtin.h"
 
-# include "minishell.h"
+char	ft_echo(char *str)
+{
+	int	option;
 
-
-/*			ECHO			*/
-char	ft_echo(char *str);
-
-
-#endif
+	option = 0;
+	if (str == NULL)
+		return (1);
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '\0')
+	{
+		putchar_fd('\n', 1);
+		return (0);
+	}
+	if (*str == '-' && *(str + 1) == 'n')
+	{
+		str += 2;
+		option = 1;
+		while (ft_isspace(*str))
+			str++;
+	}
+	put_str_fd(str, 1);
+	if (!option)
+		putchar_fd('\n', 1);
+	return (0);
+}
