de3a09b321784f385202677eba94ca9b1de6e1b8
Tapeky <tareq.sadouk@gmail.com>
2024-03-28 17:00:37 +0100

    ojectify

---
+++ src/parsing/ft_objectify.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_objectify.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 16:19:04 by tsadouk           #+#    #+#             */
+/*   Updated: 2024/03/28 16:59:17 by tsadouk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static t_priority	ft_get_priority(char *cmd)
+{
+	if (ft_strcmp(cmd, "&&") == 0 || ft_strcmp(cmd, "||") == 0)
+		return (MEDIUM);
+	if (!ft_strncmp(cmd, ">", 1) || !ft_strncmp(cmd, ">>", 2)\
+		|| !ft_strncmp(cmd, "<", 1) || !ft_strncmp(cmd, "<<", 2))
+		return (HIGH);
+	return (LOW);
+}
+
+void	ft_objectify(t_parse *parse)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	t_object	*object;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	object = malloc(sizeof(t_object) * ft_arrlen((void **)parse->cmd));
+	while (parse->cmd[i])
+	{
+		object[j].cmd = ft_strtok(parse->cmd[i], " ");
+		object[j].infile = -1;
+		object[j].outfile = -1;
+		object[j].link = PIPE;
+		object[j].priority = LOW;
+		j++;
+		i++;
+	}
+	parse->cmd = object;
+}
