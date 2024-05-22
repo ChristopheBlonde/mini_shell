f9a31333487c79b5ffbb3fa4813465df23b59e4c
Tapeky <tareq.sadouk@gmail.com>
2024-05-22 16:43:03 +0200

    NORMED : ft_echo.c

--- src/builtin/ft_echo.c
+++ src/builtin/ft_echo.c
@@ -6,38 +6,11 @@
 /*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 15:46:43 by tsadouk           #+#    #+#             */
-/*   Updated: 2024/05/22 16:34:35 by tsadouk          ###   ########.fr       */
+/*   Updated: 2024/05/22 16:42:31 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-
-// char	ft_echo(char *str)
-// {
-// 	int	option;
-
-// 	option = 0;
-// 	if (str == NULL)
-// 		return (1);
-// 	while (ft_isspace(*str))
-// 		str++;
-// 	if (*str == '\0')
-// 	{
-// 		putchar_fd('\n', 1);
-// 		return (0);
-// 	}
-// 	if (*str == '-' && *(str + 1) == 'n')
-// 	{
-// 		str += 2;
-// 		option = 1;
-// 		while (ft_isspace(*str))
-// 			str++;
-// 	}
-// 	put_str_fd(str, 1);
-// 	if (!option)
-// 		putchar_fd('\n', 1);
-// 	return (0);
-// }
 
 static void	ft_echo(t_parse *parse)
 {
@@ -47,7 +20,8 @@ static void	ft_echo(t_parse *parse)
 
 	i = 1;
 	option = 0;
-	while (parse->task[0]->cmd[i] && !ft_strncmp(parse->task[0]->cmd[i], "-n", 2))
+	while (parse->task[0]->cmd[i]
+		&& !ft_strncmp(parse->task[0]->cmd[i], "-n", 2))
 	{
 		option = 1;
 		i++;
@@ -67,11 +41,9 @@ static void	ft_echo(t_parse *parse)
 void	ft_exec_echo(t_parse *parse, t_object *task)
 {
 	if (task->builtin != ECHO)
-		return;
+		return ;
 	if (!task->cmd)
-		return;
-
+		return ;
 	ft_echo(parse);
 	ft_excmd_result(parse, 0);
 }
-
