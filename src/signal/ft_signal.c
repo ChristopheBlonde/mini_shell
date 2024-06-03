04959e1f7f95b0e9a50fa0f77466bb47078e47ec
Tapeky <tareq.sadouk@gmail.com>
2024-06-03 13:50:33 +0200

    Signals

--- src/signal/ft_signal.c
+++ src/signal/ft_signal.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 12:21:50 by cblonde           #+#    #+#             */
+/*   Updated: 2024/05/23 09:45:44 by cblonde          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void    sig_handler(int sig)
+void	sig_handler(int sig)
 {
-    if (sig == SIGINT)
-    {
-        g_exit_code = 130;
-        ft_putstr_fd("\n", 1);
+	if (sig == SIGINT)
+	{
+		g_exit_code = 130;
+		ft_putstr_fd("\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
-    }
+	}
 }
 
-void    sig_handler_heredoc(int sig)
+void	sig_handler_heredoc(int sig)
 {
 	if (sig == SIGINT)
 	{
