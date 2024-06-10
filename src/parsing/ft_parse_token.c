7760002325d571d017d4fb01e934dbf2419e9a54
Tapeky <tareq.sadouk@gmail.com>
2024-06-10 09:51:42 +0200

    added a new branch TEST

--- src/parsing/ft_parse_token.c
+++ src/parsing/ft_parse_token.c
@@ -6,7 +6,7 @@
 /*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 08:17:08 by cblonde           #+#    #+#             */
-/*   Updated: 2024/05/30 12:15:55 by cblonde          ###   ########.fr       */
+/*   Updated: 2024/06/10 09:49:52 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,25 @@ bool	ft_parse_token(t_parse *parse, char *input)
 {
 	char	**arr;
 
-	arr = ft_strtok(input, "|&\n");
+	arr = ft_strtok(input, "|&$\n");
 	if (!arr)
 		return (false);
 	if (!ft_init_tasks(parse, arr))
 	{
 		ft_free_array((void **)arr);
 		return (false);
+	}
+	int i = 0, j;
+	while (parse->task[i])
+	{
+		j = 0;
+		while (parse->task[i]->cmd[j])
+		{
+			ft_printf("task[%d]->cmd[%d]: ", i, j);
+			ft_putendl_fd(parse->task[i]->cmd[j], 1);
+			j++;
+		}
+		i++;
 	}
 	ft_objectify(parse);
 	ft_redirection(parse);
