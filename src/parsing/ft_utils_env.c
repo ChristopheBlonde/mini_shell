1f90f9cb04b0684d3a817820b413cab535114825
Tapeky <tareq.sadouk@gmail.com>
2024-06-21 00:49:03 +0200

    Home->School

--- src/parsing/ft_utils_env.c
+++ src/parsing/ft_utils_env.c
@@ -6,7 +6,7 @@
 /*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 13:31:33 by cblonde           #+#    #+#             */
-/*   Updated: 2024/06/20 22:55:54 by tsadouk          ###   ########.fr       */
+/*   Updated: 2024/06/21 00:48:45 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,23 +47,57 @@ void	ft_get_variable(t_parse *parse, char *s, t_elem *elem)
 {
 	size_t	i;
 	char	*tmp;
+	pid_t	pid;
+	char	pid_str[12];
 
 	i = 0;
 	tmp = NULL;
 	if (!s)
 		return ;
-	while (s[i] && (ft_isalnum(s[i]) ||  s[i] == '_'))
+	while (s[i] && (ft_isalnum(s[i]) || s[i] == '_'))
 		i++;
-	if (s[i] == '?')
+	if (s[i] == '?') {
 		i++;
-	if (i == 0)
+	}
+	while (s[i] == '$') {
+		i++;
+	}
+	if (i == 0 && s[i + 1] != '$') {
 		return ;
+	}
 	elem->var_len = i;
 	tmp = ft_substr(s, 0, i);
-	if (!tmp)
+	if (!tmp) {
 		return ;
-	elem->env = ft_getenv(parse, tmp);
-	if (!elem->env)
-		elem->env = ft_calloc(1, sizeof(char));
+	}
+	if (s[0] == '$') {
+		pid = getpid();
+		snprintf(pid_str, sizeof(pid_str), "%d", pid);
+		elem->env = strdup(pid_str);
+		i = 1;
+		while (s[i])
+		{
+			if (s[i] == '$' && s[i + 1] == '$')
+			{
+				elem->env = ft_strjoin(elem->env, pid_str);
+				i += 2;
+				continue ;
+			}
+			if (s[i] == '$' && s[i + 1] != '$')
+			{
+				elem->env = ft_strjoin(elem->env, "$");
+				i++;
+				continue ;
+			}
+			i++;
+		}
+	} else {
+		elem->env = ft_getenv(parse, tmp);
+		if (!elem->env) {
+			elem->env = ft_calloc(1, sizeof(char));
+		}
+	}
 	free(tmp);
 }
+
+// TODO : fix echo "$$", $ + un truc qui existe pas
